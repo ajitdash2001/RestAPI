f2a518bd0ae9fed1f3356c727499db80a4220aca
ajit.dash2 <ajit.dash2@globallogic.com>
2024-07-18 10:34:50 +0530

    initial commit

--- src/main/java/com/app/ems/repository/DepartmentRepository.java
+++ src/main/java/com/app/ems/repository/DepartmentRepository.java
@@ -4,4 +4,5 @@ import com.app.ems.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    
 }
